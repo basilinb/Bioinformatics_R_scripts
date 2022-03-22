bd5d95123a6604f299361f3929212eb1a886793e
Basilin Benson <basilinb@gmail.com>
2022-03-22 10:33:34 -0700

    Update enrichr_fun.R

--- enrichr_fun.R
+++ enrichr_fun.R
@@ -18,24 +18,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Input parameters:
 REQUIRED
-    file_name = Thsi is the file generated from extract_pval function
+    gene_mod_list = Gene list or Module object
 
 
 OPTIONAL
-  coeff = By default the function will print all coefficients from the linear model. can also specify specific coefficients e.g. c('coeff1', 'coeff2')
-  fdr = You can set the FDR correction by default it is 0.2
-  n_top_gene = number of up regulated and down regulated genes to label with hgnc symbol and color. default is 10
+  type = By default the function will run for gene list. Need to specify mod to run on modules
+  gene_id = HGNC symbol or Ensembl ID. Default is HGNC
+  dbs = Geneset database to run enrichr on. Default is MSigDB_Hallmark_2020
+  mod_of_interest = If running function on module object. Which Modules do you want to run enrichr on, by default will run on all modules.
    
 Example
-  volcano_funcxn(file_name='results/test.pval_w_hgnc.csv',
-             coeff='coeff1',
-             fdr='0.05', n_top_gene=15,mod_gene='gene',
-             out_dir='./')
+  enrichr_run(gene_mod_list = NULL, 
+  type = gene,gene_id = HGNC,dbs = c(MSigDB_Hallmark_2020),mod_of_interest = all)
 "
 
 
-# All the libraries
-enrichr_fun <- function(gene_mod_list = NULL, type = "gene",gene_id = "HGNC",dbs = c("MSigDB_Hallmark_2020"),mod_of_interest = "all") {
+
+enrichr_run <- function(gene_mod_list = NULL, type = "gene",gene_id = "HGNC",dbs = c("MSigDB_Hallmark_2020"),mod_of_interest = "all") {
+  # This loop loads all the packages needed for function, if package is not installed it will automatically do it
   for (package in c('tidyverse','enrichR','biomaRt')) {
     if (!require(package, character.only=T, quietly=T)) {
       install.packages(package)
@@ -45,31 +45,41 @@ enrichr_fun <- function(gene_mod_list = NULL, type = "gene",gene_id = "HGNC",dbs
   #Download Ensembl gene list to get HGNC symbols
   ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl")
   all_genes <- getBM(attributes=c('ensembl_gene_id','gene_biotype','hgnc_symbol'), mart = ensembl) %>% filter(gene_biotype == "protein_coding")
+  #Setting the website for Enrichr to run
   setEnrichrSite("Enrichr")
+  #Defining two empty lists 
   enrich_list <- list()
   gene_list <- list()
   
+  #Checking if user wants to run on gene list or modules and formatting the data
   if (type!="gene"){
     if(mod_of_interest == "all"){
       mod_of_interest = gene_mod_list$mods %>% pull(module) %>% unique() %>% sort()
     }
   } else {
+    # Check to see if its HGNC symbol or Ensembl ID and formatting the data 
     if (gene_id == "HGNC") {
       gene_list <- gene_mod_list
     }else{
       gene_list <- all_genes %>% filter(ensembl_gene_id %in% gene_mod_list) %>% pull(hgnc_symbol)
     }
   }
-  
+  #Loop through the databases provided by user
   for (d in dbs){
   if (type == "gene"){
+    #Running Enrichr
     enriched <- enrichr(gene_list,d)
+    # Append to our main list we want to return, So if we provide multiple databases this will append them to the main list. 
     enrich_list <-  append(enrich_list,enriched)
   }
     else {
+      #Loop through the modules and genes within the modules
       for (m in mod_of_interest) {
-        gene_list <- gene_mod_list$mods %>% subset(module == m) %>% pull(hgnc_symbol) 
+        #Getting gene list from the module
+        gene_list <- gene_mod_list$mods %>% subset(module == m) %>% pull(hgnc_symbol)
+        #Running Enrichr 
         enriched <- enrichr(gene_list,d)
+        # Append to our main list we want to return, So if we provide multiple databases this will append them to the main list.
         enrich_list[[paste("module", m,sep="_")]] <- enriched
       }
     }
