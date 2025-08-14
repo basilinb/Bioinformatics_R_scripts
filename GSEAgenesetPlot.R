f620140d2ac3894fd95abbc5053031a59c5e6f7c
Basilin Benson <basilinb@gmail.com>
2025-08-14 16:24:33 -0400

    Enhance GSEAgenesetPlot function to support custom gene set databases and improve error handling

--- GSEAgenesetPlot.R
+++ GSEAgenesetPlot.R
@@ -33,23 +33,50 @@
 #                                             left_label = "NSE",right_label = "SE",
 #                                             highlight_gene = "CXCL10",left_label_col = "darkred",
 #                                             right_label_col = "black")
-
+# plt_gsea_ifn_alpha_test <- run_gsea_analysis(FC.ls_Severe_vs_Non_baseline$`Severe vs Non Severe `, 
+#                                             custom_db = customDatabase, 
+#                                             geneset_name = "custom_geneset",
+#                                             left_label = "NSE",right_label = "SE",
+#                                             highlight_gene = "CXCL10",left_label_col = "darkred",
+#                                             right_label_col = "black")
+# This is what a custom database should look like:
+#cluster_gs
+#    gs_name       gene_symbol
+#    Cluster_1      TSPAN6
+#    Cluster_2        DPM1
+#    Cluster_3       SCYL3
 # Print or save plot
 #print(plt_gsea_ifn_alpha)
 
-
-
-GSEAgenesetPlot <- function(FC_list, geneset_db = "H", geneset_name, 
-                              right_label = "Upregulated", left_label = "Downregulated", 
-                              highlight_gene = NULL,right_label_col = "red",left_label_col="blue") {
+GSEAgenesetPlot <- function(FC_list, 
+                            geneset_db = "H", 
+                            geneset_name, 
+                            custom_db = NULL,
+                            right_label = "Upregulated", 
+                            left_label = "Downregulated", 
+                            highlight_gene = NULL,
+                            right_label_col = "red",
+                            left_label_col = "blue") {
   require(ggplot2)
   require(msigdbr)
   require(fgsea)
   require(dplyr)
-  # Load the gene set database using msigdbr
-  temp_gmt <- msigdbr::msigdbr(species = "Homo sapiens", category = geneset_db) %>%
-    split(.$gs_name ) %>% 
-    lapply(function(x) x$gene_symbol)
+  require(stringr)
+  require(patchwork)
+  
+  # Load gene set database
+  if (!is.null(custom_db)) {
+    if (!all(c("gs_name", "gene_symbol") %in% colnames(custom_db))) {
+      stop("Custom database must have columns: gs_name and gene_symbol")
+    }
+    temp_gmt <- custom_db %>%
+      split(.$gs_name) %>%
+      lapply(function(x) x$gene_symbol)
+  } else {
+    temp_gmt <- msigdbr::msigdbr(species = "Homo sapiens", collection = geneset_db) %>%
+      split(.$gs_name) %>%
+      lapply(function(x) x$gene_symbol)
+  }
   
   if (!(geneset_name %in% names(temp_gmt))) {
     stop("Gene set not found in the database. Check spelling or availability.")
@@ -60,13 +87,12 @@ GSEAgenesetPlot <- function(FC_list, geneset_db = "H", geneset_name,
   
   # Run enrichment plot
   plt_enrich <- fgsea::plotEnrichment(temp_gmt[[geneset_name]], ranks) + 
-    labs(title = str_to_title(gsub("_"," ",geneset_name))) +xlab("Rank") + ylab("Enrichment Score")
+    labs(title = str_to_title(gsub("_", " ", geneset_name))) +
+    xlab("Rank") + ylab("Enrichment Score")
   
-  # If a gene is highlighted, find its rank and add annotation
+  # If a gene is highlighted, add annotation
   if (!is.null(highlight_gene) && highlight_gene %in% names(ranks)) {
     gene_rank <- which(names(ranks) == highlight_gene)
-    gene_stat <- ranks[highlight_gene]
-    
     plt_enrich <- plt_enrich +
       annotate("segment", x = gene_rank, xend = gene_rank + 60, 
                y = 0, yend = 0.04, colour = "black") +
@@ -77,7 +103,6 @@ GSEAgenesetPlot <- function(FC_list, geneset_db = "H", geneset_name,
   # Run enrichment statistics
   fgsea_res <- fgsea::plotEnrichmentData(pathway = temp_gmt[[geneset_name]], stats = ranks)
   
-  # Custom visualization
   mean_stat <- mean(fgsea_res$stats$stat)
   sd_stat <- sd(fgsea_res$stats$stat)
   
@@ -85,10 +110,11 @@ GSEAgenesetPlot <- function(FC_list, geneset_db = "H", geneset_name,
     ggplot(aes(x = rank, y = stat)) + 
     geom_bar(stat = "identity", color = "black") + 
     theme_classic() + 
-    geom_vline(xintercept = median(fgsea_res$stats$rank), linetype = 2, color = "grey25") +xlab("Rank")+ ylab("Statistic")
+    geom_vline(xintercept = median(fgsea_res$stats$rank), linetype = 2, color = "grey25") +
+    xlab("Rank") + ylab("Statistic")
   
   # Calculate range for positioning labels
-  xpos<-quantile(fgsea_res$stats$rank,probs = seq(0, 1, 0.10))
+  xpos <- quantile(fgsea_res$stats$rank, probs = seq(0, 1, 0.10))
   
   n <- fgsea_res$stats %>%
     mutate(z_score = (stat - mean_stat) / sd_stat) %>%
@@ -99,7 +125,8 @@ GSEAgenesetPlot <- function(FC_list, geneset_db = "H", geneset_name,
     geom_text(aes(x = xpos[[2]], y = 1, label = left_label),
               color = left_label_col, fontface = "bold", size = 5, check_overlap = TRUE) +
     geom_text(aes(x = xpos[[10]], y = 1, label = right_label),
-              color = right_label_col, fontface = "bold", size = 5, check_overlap = TRUE) +xlab("Rank")
+              color = right_label_col, fontface = "bold", size = 5, check_overlap = TRUE) +
+    xlab("Rank")
   
   # Layout for combined plot
   des <- "
@@ -113,7 +140,7 @@ GSEAgenesetPlot <- function(FC_list, geneset_db = "H", geneset_name,
     333
   "
   
-  combined_plot <- plt_enrich + n + m + plot_layout(nrow = 3, design = des,axes = "collect")
+  combined_plot <- plt_enrich + n + m + plot_layout(nrow = 3, design = des, axes = "collect")
   
   return(combined_plot)
 }
